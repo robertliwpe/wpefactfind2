5a77fe8a8ae0ab068a0707bf393af233c5d0246b
Robert Li <51431603+robertliwpe@users.noreply.github.com>
2022-09-02 14:22:09 +1000

    dbsizeinit var update

--- factfind2-complete.sh
+++ factfind2-complete.sh
@@ -122,7 +122,7 @@ for i in $installs;
                 
                 dbsizeinit=0;
                 while [[ $dbsizeinit == 0 ]]; do
-                    dbsizeinit=$(wp db query --skip-plugins --skip-themes "SELECT SUM(round(((data_length + index_length) / 1024 / 1024) , 2)) FROM information_schema.TABLES;" 2>/dev/null | tail -1 | bc);
+                    dbsizeinit=$(wp db query --skip-plugins --skip-themes "SELECT SUM(round(((data_length + index_length) / 1024 / 1024) , 2)) FROM information_schema.TABLES;" | tail -1 | bc 2>/dev/null);
                     if [ -z "${dbsizeinit}" ] 
                         then
                             dbsizeinit=0;
@@ -131,13 +131,6 @@ for i in $installs;
                     fi;
                     sleep 1;
                 done
-
-                #if [ -z "$dbsizeinit" ] 
-                    #then 
-                        #dbsize=$(echo 0 | bc); 
-                    #else 
-                        #dbsize=$(echo ${dbsizeinit} | bc); 
-                #fi;
                 
                 echo "Size of Database: " ${dbsize} "MB"; 
                 dbtotal=$(echo ${dbtotal} + ${dbsize} | bc);
