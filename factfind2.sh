19917f8e578f3fc22adb04c47ed5942690542c97
Robert Li <51431603+robertliwpe@users.noreply.github.com>
2022-09-02 10:49:35 +1000

    update dbsize var handle error

--- factfind2.sh
+++ factfind2.sh
@@ -81,7 +81,13 @@ for i in $installs;
                 fi;
 
                 echo "Size of Filesystem: " $diskprintout; 
-                dbsizeinit=$(wp db query --skip-plugins --skip-themes "SELECT SUM(round(((data_length + index_length) / 1024 / 1024) , 2)) FROM information_schema.TABLES;" 2>/dev/null | tail -1 | bc);
+                rettest=1;
+                until [ ${rettest} -eq 0 ]; do
+                    dbsizeinit=$(wp db query --skip-plugins --skip-themes "SELECT SUM(round(((data_length + index_length) / 1024 / 1024) , 2)) FROM information_schema.TABLES;" 2>/dev/null | tail -1 | bc);
+                    rettest=$?;
+                    sleep 1;
+                done                
+                #dbsizeinit=$(wp db query --skip-plugins --skip-themes "SELECT SUM(round(((data_length + index_length) / 1024 / 1024) , 2)) FROM information_schema.TABLES;" 2>/dev/null | tail -1 | bc);
                 if [ -z "$dbsizeinit" ]; then dbsize=$(echo 0 | bc); else dbsize=$(echo $dbsizeinit | bc); fi                
                 echo "Size of Database: " $dbsize "MB"; 
                 dbtotal=$(echo $dbtotal + $dbsize | bc);
